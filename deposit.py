f1d1e0e1d6eacaf4d6e44d2c2cef1052daff05b8
Vaibhav Ganesh Deshpande <135240501+VaibhavGDeshpande@users.noreply.github.com>
2023-11-28 05:53:22 +0000

    Adding deposit.py

---
+++ deposit.py
@@ -0,0 +1,24 @@
+def deposit(num):
+    global balance
+    balance=balance+num
+def withdrawal(num):
+    global balance
+    if(balance>0):
+        balance=balance-num
+    else:
+        print("You cannot Withdraw because the Balance is Less")
+
+lst=[]
+balance=0
+while True:
+    data=input("Enter the Amount...")
+    if(data=='Q'):
+        break
+lst.append(data.split())
+lst
+for i in lst:
+    if(i[0]=='D'):
+        deposit(int(i[1]))
+    elif(i[0]=='W'):
+        withdrawal(int(i[1]))
+
