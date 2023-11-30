0f2baebb20f4395b0220d8a76f806158e1c5c18b
Vaibhav Ganesh Deshpande <135240501+VaibhavGDeshpande@users.noreply.github.com>
2023-11-30 23:57:08 +0530

    Update Prac3_deposit.py

--- Prac3_deposit.py
+++ Prac3_deposit.py
@@ -1,23 +1,27 @@
 def deposit(num):
     global balance
-    balance=balance+num
+    balance = balance + num
+
 def withdrawal(num):
     global balance
-    if(balance>0):
-        balance=balance-num
+    if balance > 0:
+        balance = balance - num
     else:
-        print("You cannot Withdraw because the Balance is Less")
+        print("You cannot withdraw because the balance is less.")
 
-lst=[]
-balance=0
+lst = []
+balance = 0
+
 while True:
-    data=input("Enter the Amount...")
-    if(data=='Q'):
+    data = input("Enter the amount (Type 'Q' to quit): ")
+    if data == 'Q':
         break
     lst.append(data.split())
+
 for i in lst:
-    if(i[0]=='D'):
+    if i[0] == 'D':
         deposit(int(i[1]))
-    elif(i[0]=='W'):
+    elif i[0] == 'W':
         withdrawal(int(i[1]))
 
+print("The final balance is:", balance)
