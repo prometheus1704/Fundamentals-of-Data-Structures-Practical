f09e7aaf8934ccd2ad5aa4a00b63980a33a5b097
Vaibhav Ganesh Deshpande <135240501+VaibhavGDeshpande@users.noreply.github.com>
2023-12-11 21:08:58 +0530

    Update Prac3_deposit.py

--- Prac3_deposit.py
+++ Prac3_deposit.py
@@ -1,31 +1,33 @@
 def deposit(num):
     global balance
-    balance = balance + num
+    balance +=num
+
 
 def withdrawal(num):
-    global balance
-    if balance >= num:  # Allowing withdrawal even if the balance is zero
-        balance = balance - num
-    else:
-        print("Insufficient funds for withdrawal.")
+    global balance 
 
-lst = []
-balance = 0
+    if balance>=num:
+        balance-=numD
+    else:
+        print("Not enough balance")
+        
+
+lst=[]
+balance=0
 
 while True:
-    data = input("Enter the amount (Type 'Q' to quit): ")
-    if data == 'Q':
+    print("Enter")
+    data=input()
+    if data=='Q':
         break
-    lst.append(data.split())
 
-for i in lst:
-    if len(i) != 2 or not i[1].isdigit():  # Check if input format is valid
-        print("Invalid input format:", i)
-        continue
 
-    if i[0] == 'D':
-        deposit(int(i[1]))
-    elif i[0] == 'W':
-        withdrawal(int(i[1]))
+    lst=data.split()
 
-print("The final balance is:", balance)
+    if lst[0]=='D':
+        deposit(int(lst[1]))
+
+    elif lst[0]=='W':
+        withdrawal(int(lst[1]))
+
+print(balance)
